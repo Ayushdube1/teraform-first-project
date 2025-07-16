f660de1c1d7ebce5cb0f5ae675833ff876460af5
ayush dubey <ayush.dube48@gmail.com>
2025-07-16 09:26:44 +0000

    change sdone

--- resource.tf
+++ resource.tf
@@ -2,3 +2,24 @@
   name     = "terra"
   location = "East US"
 }
+
+
+
+resource "azurerm_resource_group" "storeterra" {
+    name     = "example-resources"
+    location = "East US"
+}
+ 
+resource "azurerm_storage_account" "ayushsaterra" {
+    name                     = "terrastorage123"
+    resource_group_name      = azurerm_resource_group.storeterra.name
+    location                 = azurerm_resource_group.storeterra.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+ 
+resource "azurerm_storage_container" "sacontainer" {
+    name                  = "terra-container"
+    storage_account_name  = azurerm_storage_account.ayushsaterra.name
+    container_access_type = "private"
+}
