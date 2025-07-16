f7634a65d05c9f057010357bc12c0330ba56513e
ayush dubey <ayush.dube48@gmail.com>
2025-07-16 10:08:22 +0000

    last time trying

--- resource.tf
+++ resource.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "storeterra" {
 }
  
 resource "azurerm_storage_account" "ayushsaterra" {
-    name                     = "ayushdube4488333"
+    name                     = "ayushdube44883333"
     resource_group_name      = azurerm_resource_group.storeterra.name
     location                 = azurerm_resource_group.storeterra.location
     account_tier             = "Standard"
@@ -22,4 +22,25 @@ resource "azurerm_storage_container" "sacontainer" {
     name                  = "terra-container"
     storage_account_name  = azurerm_storage_account.ayushsaterra.name
     container_access_type = "private"
+}
+
+resource "azurerm_virtual_network" "terra_vnet" {
+    name                = "terra-vnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.terra.location
+    resource_group_name = azurerm_resource_group.terra.name
+}
+ 
+resource "azurerm_subnet" "subnet1" {
+    name                 = "subnet1"
+    resource_group_name  = azurerm_resource_group.terra.name
+    virtual_network_name = azurerm_virtual_network.terra_vnet.name
+    address_prefixes     = ["10.0.1.0/24"]
+}
+ 
+resource "azurerm_subnet" "subnet2" {
+    name                 = "subnet2"
+    resource_group_name  = azurerm_resource_group.terra.name
+    virtual_network_name = azurerm_virtual_network.terra_vnet.name
+    address_prefixes     = ["10.0.2.0/24"]
 }
